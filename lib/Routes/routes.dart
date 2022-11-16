83498fd790ce2dfc0f0229c915a43a2a78139903
Anderson-Tello <89850410+Anderson-Tello@users.noreply.github.com>
2022-11-15 23:37:47 -0500

    Update routes.dart

--- lib/Routes/routes.dart
+++ lib/Routes/routes.dart
@@ -5,7 +5,8 @@ import '../Screens/home_screen.dart';
 import '../Screens/inicio.dart';
 import '../Screens/login.dart';
 import '../Screens/perfil.dart';
-import '../Screens/tienda.dart';
+import '../screens/Store_screen.dart';
+import '../screens/carrito_screen.dart';
 
 class AppRouter {
   static const rutaInicial = "home";
@@ -20,5 +21,12 @@ class AppRouter {
     'heroes': (BuildContext context) => const Heroes(),
     'tienda': (BuildContext context) => const Tienda(),
     'perfil': (BuildContext context) => const Perfil(),
+    "inicio": (BuildContext context) => StoreScreen(),
+    "carrito": (BuildContext context) => Carrito()
   };
 }
+
+
+
+
+ 
