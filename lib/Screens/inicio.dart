64f9bd25e77567293a7b3e0e06eb45691d7d9259
JhonatanCT <71463784@certus.edu.pe>
2022-11-15 22:09:15 -0500

    Navegación con screen heroes

--- lib/Screens/inicio.dart
+++ lib/Screens/inicio.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'heroes.dart';
 
 class Inicio extends StatelessWidget {
   const Inicio({super.key});
@@ -7,7 +8,51 @@ class Inicio extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Inicio")
+        backgroundColor: Colors.red,
+        title: const Text(
+          "Inicio",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        elevation: 10,
+      ),
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                width: 500,
+                height: 800,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      height: 40,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            )),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Heroes()));
+                        },
+                        child: const Text(
+                          "Ir a heroes",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
